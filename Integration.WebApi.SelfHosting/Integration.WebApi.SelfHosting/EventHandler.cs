afc2ee3aaa3bea25f917b4f8547d106bacd962a0
Paul Houlston <paul.houlston@parasolgroup.co.uk>
2016-04-30 20:43:14 +0100

    Refactored event handler to deserialize the message

---
+++ Integration.WebApi.SelfHosting/Integration.WebApi.SelfHosting/EventHandler.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Integration.WebApi.SelfHosting
+{
+    class EventHandler
+    {
+        public EventHandler(IModel channel)
+        {
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += HandleEvent;
+            channel.BasicConsume(queue: "hello", noAck: true, consumer: consumer);
+        }
+
+        private static void HandleEvent(object model, BasicDeliverEventArgs ea)
+        {
+            var message = JsonConvert.DeserializeObject<Message>(Encoding.UTF8.GetString(ea.Body));
+
+            Console.WriteLine("Received event type '{0}'", message.EventType);
+        }
+    }
+}
--- Integration.WebApi.SelfHosting/Integration.WebApi.SelfHosting/Program.cs
+++ Integration.WebApi.SelfHosting/Integration.WebApi.SelfHosting/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Text;
-using RabbitMQ.Client;
-using RabbitMQ.Client.Events;
 
 namespace Integration.WebApi.SelfHosting
 {
@@ -17,21 +14,6 @@ namespace Integration.WebApi.SelfHosting
                 Console.WriteLine(" Press [enter] to exit.\r\n\r\n");
                 Console.ReadLine();
             }
-        }
-    }
-
-    class EventHandler
-    {
-        public EventHandler(IModel channel)
-        {
-            var consumer = new EventingBasicConsumer(channel);
-            consumer.Received += HandleEvent;
-            channel.BasicConsume(queue: "hello", noAck: true, consumer: consumer);
-        }
-
-        private static void HandleEvent(object model, BasicDeliverEventArgs ea)
-        {
-            Console.WriteLine(" [x] Received {0}", Encoding.UTF8.GetString(ea.Body));
         }
     }
 }
