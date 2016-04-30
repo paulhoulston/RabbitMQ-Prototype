a6b2068e5ee2d8325ed146ddb47f6503ce4301af
Paul Houlston <paul.houlston@parasolgroup.co.uk>
2016-04-30 16:57:00 +0100

    Added endpoint to pick up messages and publish them to RabbitMQ

--- Integration.Receiver/Integration.Receiver/MessagesController.cs
+++ Integration.Receiver/Integration.Receiver/MessagesController.cs
@@ -1,4 +1,9 @@
-﻿using System.Web.Http;
+﻿using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
 
 namespace Integration.Receiver
 {
@@ -8,5 +13,36 @@ namespace Integration.Receiver
         {
             return "Test";
         }
+
+        public HttpResponseMessage Post([FromBody]Message message)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+
+                    var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                }
+            }
+
+            return Request.CreateResponse(HttpStatusCode.NoContent);
+        }
+    }
+
+    public class Message
+    {
+        public string Name { get; set; }
     }
 }
