9229e962c6abab0cc2710e9aac84875c5f897d7c
Paul Houlston <paul.houlston@parasolgroup.co.uk>
2016-04-30 19:11:57 +0100

    Moved event received method to specific method rather than lambda function

--- Integration.WebApi.SelfHosting/Integration.WebApi.SelfHosting/Program.cs
+++ Integration.WebApi.SelfHosting/Integration.WebApi.SelfHosting/Program.cs
@@ -14,15 +14,17 @@ namespace Integration.WebApi.SelfHosting
             using (var rabbitMQ = new RabbitMQClient())
             {
                 var consumer = new EventingBasicConsumer(rabbitMQ.Channel);
-                consumer.Received += (model, ea) =>
-                {
-                    Console.WriteLine(" [x] Received {0}", Encoding.UTF8.GetString(ea.Body));
-                };
+                consumer.Received += HandleEvent;
                 rabbitMQ.Channel.BasicConsume(queue: "hello", noAck: true, consumer: consumer);
 
                 Console.WriteLine(" Press [enter] to exit.");
                 Console.ReadLine();
             }
+        }
+
+        private static void HandleEvent(object model, BasicDeliverEventArgs ea)
+        {
+            Console.WriteLine(" [x] Received {0}", Encoding.UTF8.GetString(ea.Body));
         }
 
         private static string GetBaseAddress()
