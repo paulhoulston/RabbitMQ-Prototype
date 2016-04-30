b528b2705735f73146ffd79a146c8743bc77a2d2
Paul Houlston <paul.houlston@parasolgroup.co.uk>
2016-04-30 16:47:19 +0100

    Added empty project with test controller

---
+++ Integration.Receiver/Integration.Receiver/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Net.Http.Formatting;
+using System.Web;
+using System.Web.Http;
+using Newtonsoft.Json.Serialization;
+
+namespace Integration.Receiver
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Formatters.OfType<JsonMediaTypeFormatter>().First().SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Integration.Receiver/Integration.Receiver/MessagesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Integration.Receiver
+{
+    public class MessagesController : ApiController
+    {
+        public string Get()
+        {
+            return "Test";
+        }
+    }
+}
